8ffadcadaa7eba87eec66b01ca06954d724288b9
mayoi_Akira <akane36@163.com>
2025-06-12 12:38:57 +0800

    use ResNet

--- model_load.py
+++ model_load.py
@@ -1,51 +1,56 @@
 import torch
-from torchvision import transforms
 import torch.nn.functional as F
 import torch.nn as nn
 import string
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
 CHARSET = string.digits + string.ascii_lowercase
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 idx2char = {i: c for i, c in enumerate(CHARSET)}
-modelpath = '8302.pth'
+
+modelpath = 'ResNet95.pth'
+import torchvision.models as models
+
+from torchvision import transforms
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
 
 
-class CaptchaCNN(nn.Module):
+class Captcha(nn.Module):
 
-    def __init__(self):
+    def __init__(self, num_classes=len(CHARSET), pretrained=True):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
-        self.pool1 = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.pool2 = nn.MaxPool2d(2)
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
-        self.pool3 = nn.MaxPool2d(2)
-        self.fc = nn.Linear(128 * 4 * 11, 512)
-        self.heads = nn.ModuleList(
-            [nn.Linear(512, len(CHARSET)) for _ in range(4)])
+        backbone = models.resnet18(pretrained=pretrained)
+        self.features = nn.Sequential(*list(backbone.children())[:-1])
+        self.fc_shared = nn.Linear(512, 512)
+        self.head0 = nn.Linear(512, num_classes)
+        self.head1 = nn.Linear(512, num_classes)
+        self.head2 = nn.Linear(512, num_classes)
+        self.head3 = nn.Linear(512, num_classes)
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.features(x)
         x = x.view(x.size(0), -1)
-        x = F.relu(self.fc(x))
-        return tuple(head(x) for head in self.heads)
+        x = nn.functional.relu(self.fc_shared(x))
+        out0 = self.head0(x)
+        out1 = self.head1(x)
+        out2 = self.head2(x)
+        out3 = self.head3(x)
+        return out0, out1, out2, out3
 
 
 def load():
-    model = CaptchaCNN().to(device)
+    model = Captcha().to(device)
     orig_state = torch.load(modelpath, map_location=device)
     new_state = {}
 
     for k, v in orig_state.items():
         if k.startswith('head'):
             num, rest = k[4], k[5:]
-            new_key = f'heads.{num}{rest}'
+            new_key = f'head{num}{rest}'
             new_state[new_key] = v
         else:
             new_state[k] = v
